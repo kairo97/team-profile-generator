0887d4cbeaf534a7832ed67325bec8d395ecc65e
Kairo97 <109006341+kairo97@users.noreply.github.com>
2023-01-26 12:36:47 -0800

    applied several inquirer prompts

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const inquirer = require("inquirer");
+const generateHTML = require("./util/generateHTML");
+const fs = require('fs')
 const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+const { tSExpressionWithTypeArguments } = require("@babel/types");
 const employees = [];
 
 function askQuestion(){
@@ -10,6 +13,35 @@ function askQuestion(){
         {
             name: "question",
             type: "list",
+            message: "would you like to add a team member?",
+            choices: ["yes", "no"]
+        },{
+            name: "team-member",
+            type: "list",
+            message: "what role would you like to add?",
+            choices: ["Intern", "Engineer", "Manager"]
+        },{
+            name:"member-name",
+            type:"input",
+            message:"what is this team member's name?"
+        },{
+            name:"school",
+            type:"input",
+            message: "what school did this Intern go to?",
+            when: function(answers){
+                answers.team-member === "Intern"
+            }
+        },{
+            name: "github",
+            type: "input",
+            message: "what is the github username of this Engineer?",
+            when: function(answers){
+                answers.team-member === "Engineer"
+            }
+        },{
+            name: "officeNumber",
+            type: "input",
+            message: "what is the offic enumber of this manager?",
         }
     ])
 }
