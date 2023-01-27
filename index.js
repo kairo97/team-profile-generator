1aa93247cfa9bc5ae158e3be43acaeed05fcc9f2
Kairo97 <109006341+kairo97@users.noreply.github.com>
2023-01-26 18:27:50 -0800

    resolved issue regarding school selection for intern

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-const employees = [new Employee("Alice")];
+const team = [];
 
 function askQuestion() {
   inquirer
     .prompt([
       {
         name: "question",
-        type: "list",
-        message: "would you like to add a team member?",
-        choices: ["yes", "no"],
+        type: "confirm",
+        message: "would you like to add a team member?"
       },
       {
-        name: "team-member",
+        name: "teamMember",
         type: "list",
         message: "what role would you like to add?",
         choices: ["Intern", "Engineer", "Manager"],
+        when: (answers) => answers.question
       },
     ])
     .then((answers) => {
-      switch (answers.team - member) {
+      switch (answers.teamMember) {
         case "Intern":
           console.log("add Intern");
           addIntern();
@@ -43,24 +43,40 @@ function askQuestion() {
 }
 function addIntern() {
     inquirer.prompt([{
-        name: "name",
-        message: "what is your Intern's name?",
+        name: "id",
+        message: "what is your Intern's ID number??",
         type: "input"
+    },{
+        name: "email",
+        type: "input",
+        message: "what is your intern's email?"
+    },{
+        name: "name",
+        type: "input",
+        message: "what is your intern's name?"
     },{
         name: "school",
         type: "input",
-        message: "what school did this Intern attend?"
+        message: "what school did your intern go to?"
     }
-]).then(({name, school}) => {
-        console.log(name, school);
-        const intern = new Intern(name, school);
-        employees.push(intern)
-        console.log(employees);
+]).then(({id, email, name,school}) => {
+        console.log(id, email, name, school);
+        const intern = new Intern(id, email, name, school);
+        team.push(intern)
+        console.log(team);
         askQuestion();
     })
 }
 function addEngineer(){
     inquirer.prompt([{
+        name: "id",
+        type: "input",
+        message: "what is your engineer's id number?"
+    },{
+        name: "email",
+        type: "input",
+        message: "what is your engineer's email?"
+    },{
         name: "name",
         type: "input",
         message: "what is the name of your Engineer?"
@@ -69,16 +85,24 @@ function addEngineer(){
         type: "input",
         message: "what is this Engineer's github username?"
     }
-]).then(({name,github}) => {
-    console.log(name, github);
-    const engineer = new Engineer(name, github);
-    employees.push(engineer);
-    console.log(employees);
+]).then(({id,email,name,github}) => {
+    console.log(id,email,name,github);
+    const engineer = new Engineer(id,email,name,github);
+    team.push(engineer);
+    console.log(team);
     askQuestion();
 })
 }
 function addManager(){
     inquirer.prompt([{
+        name: "id",
+        type: "input",
+        message: "what is your manager's ID number?"
+    },{
+        name:"email",
+        type:"input",
+        message:"what is your manager's email?"
+    },{
         name: "name",
         type: "input",
         message: "what is the name of your Manager?"
@@ -87,14 +111,19 @@ function addManager(){
         type: "input",
         message: "what is your Manager's office number?"
     }
-    ]).then(({name,officeNum}) => {
-        console.log(name, officeNum);
-        const manager = new Manager(name, officeNum);
-        employees.push(manager);
-        console.log(employees);
+    ]).then(({id,email,name,officeNum}) => {
+        console.log(id,email,name,officeNum);
+        const manager = new Manager(id,email,name,officeNum);
+        team.push(manager);
+        console.log(team);
         askQuestion();
     })
 }
-function 
+const makePage = () => {
+    fs.writeFile("../dist/index.html", generateHTML(team), (err) =>
+    err
+    ?console.error(err)
+    : console.log("your team has been generated"))
+}
 
-askQuestion();
+askQuestion()
